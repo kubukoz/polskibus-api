8f2951607ceb88b002897234357e57a1e747c051
Jakub Kozłowski <kubukoz@gmail.com>
2015-12-16 16:27:42 +0100

    CityPair now contains the whole start city. Increase abstraction of RouteProvider.

--- src/main/scala/Yolo.scala
+++ src/main/scala/Yolo.scala
@@ -1,16 +1,17 @@
 import java.time.LocalDateTime
 
 import ServerConfig._
-import play.api.libs.json.JsValue
+import play.api.libs.json.{JsArray, JsObject, Json, JsValue}
 
 import scala.io.Source
 
 object Yolo {
   def main(args: Array[String]): Unit = {
-    val routesProvider = new MockRoutesProvider
+    val routesProvider: RoutesProvider = new MockRoutesProvider
 
-
-    routesProvider.getRoutes foreach println
+    routesProvider.getRoutes foreach { case CityPair(city, targets) =>
+      println(s"${city.name}:\n" + targets.map("\t\t" + _.name).mkString("\n"))
+    }
   }
 }
 
@@ -18,38 +19,54 @@ object ServerConfig {
   val CityPairSearchTreshold = 20
   val CityPairVarName = "cityPair"
   val CityPairVarDefinition = s"var $CityPairVarName"
+
+  val RouteSourceUrl = "http://booking.polskibus.com/pricing/selections?lang=PL"
 }
 
-class MockRoutesProvider {
-  def getStuff = {
-    val z = Source.fromFile("routes.xml").getLines
-      .dropWhile(!_.take(CityPairSearchTreshold)
-        .contains(CityPairVarDefinition)).take(2).map(_.split(" = ")(1)).toList
-    println(s"downloaded $z\n")
-    z
+trait RoutesProvider {
+  def routeSource: Source
+
+  protected def getStuff: List[String] = routeSource.getLines
+    .dropWhile(!_.take(CityPairSearchTreshold).contains(CityPairVarDefinition))
+    .take(2).map(_.split(" = ")(1)).toList
+
+  protected def getPartialPairs = (Json.parse(getStuff.head.drop(1).dropRight(2)): @unchecked) match {
+    case JsObject(pairs) =>
+      pairs.map { case (id, cities) =>
+        (CityId(id.toInt), cities.as[JsArray].value.flatMap(City.from))
+      }
+    case _ => Nil
   }
 
-  import play.api.libs.json._
-
-  def getRoutes = (Json.parse(getStuff.head.drop(1).dropRight(2)): @unchecked) match {
-    case JsObject(pairs) => pairs.map { case (id, cities) =>
-      CityPair(CityId(id.toInt), cities.as[JsArray].value.flatMap(City.from))
-    case _ => Nil
+  def getRoutes = {
+    val partialPairs = getPartialPairs
+    val allCities = partialPairs.flatMap(_._2.map(_.lift)).toMap
+    getPartialPairs.map { case (id, cities) =>
+      CityPair(allCities(id), cities)
     }
   }
+}
 
+class RealRoutesProvider extends RoutesProvider {
+  override def routeSource: Source = Source.fromURL(RouteSourceUrl)
+}
+
+class MockRoutesProvider extends RoutesProvider {
+  override def routeSource: Source = Source.fromFile("routes.xml")
 }
 
 case class CityId(id: Int) extends AnyVal
 
-case class City(id: CityId, name: String)
+case class City(id: CityId, name: String) {
+  def lift = id -> this
+}
 
 object City {
   def from(jsPair: JsValue): Option[City] =
-    ((jsPair \ "First").asOpt[String].map(_.toInt) zip (jsPair \ "Second").asOpt[String]).headOption
-      .map { case (id, name) => City(CityId(id), name) }
+    ((jsPair \ "First").asOpt[String].map(_.toInt) zip (jsPair \ "Second").asOpt[String])
+      .map { case (id, name) => City(CityId(id), name) }.headOption
 }
 
-case class CityPair(id: CityId, targets: Seq[City])
+case class CityPair(start: City, targets: Seq[City])
 
 case class RouteDateLimits(from: CityId, to: CityId, firstDate: LocalDateTime, lastDate: LocalDateTime)
